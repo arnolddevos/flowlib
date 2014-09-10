3beef82a80a30f2647e78f5681e92fb4a0b92781
Arnold deVos <adv@langdale.com.au>
2014-09-10 17:07:52 +1000

    an example of the wiring DSL

---
+++ src/test/scala/example/HigherOrder.scala
@@ -0,0 +1,23 @@
+trait HigherOrder {
+  import flowlib._
+  import Wiring._
+  import Gate._
+
+  type Input[A] = Process[A]
+  type Output[A] = A => Process[Unit]
+
+  type Row
+  type Sum
+ 
+  def foldRows: Input[Row] => Output[Sum] => Process[Nothing]
+  def generateRows: Output[Row] => Process[Nothing]
+
+  val backlog: Int
+  val rows = channel[Row](backlog)
+  val sums = channel[Sum](backlog)
+
+  Site run {
+    generateRows :-> rows &
+    rows ->: foldRows :-> sums
+  } 
+}
