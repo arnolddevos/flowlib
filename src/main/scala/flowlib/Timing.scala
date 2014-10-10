fdec4ae739f4f22d9392647d31d2766d3409ecf9
Arnold deVos <adv@langdale.com.au>
2014-10-10 13:21:07 +1100

    simplify the timer utilities

--- src/main/scala/flowlib/Timing.scala
+++ src/main/scala/flowlib/Timing.scala
@@ -15,19 +15,15 @@ object Timing {
   def at( time: Long): Process[Unit] = 
     Process.waitFor(k => timer.schedule(k(()), new Date(time)))
 
-  trait Ticker {
-    def nextTick: Process[Long]
-  }
-
-  def repeatAfter( delay: Long, period: Long) = new Ticker {
-    private val b = Gate.barrier()
+  def repeatAfter( delay: Long, period: Long): Process[Long] = {
+    val b = Gate.barrier()
     timer.schedule(b signal (()), delay, period)
-    def nextTick = Process.waitFor(b.take)
+    Process.waitFor(b.take)
   }
 
-  def repeatAt[T]( time: Long, period: Long) = new Ticker {
-    private val b = Gate.barrier()
+  def repeatAt[T]( time: Long, period: Long): Process[Long] = {
+    val b = Gate.barrier()
     timer.scheduleAtFixedRate(b signal (()), new Date(time), period)
-    def nextTick = Process.waitFor(b.take)
+    Process.waitFor(b.take)
   }
 }
