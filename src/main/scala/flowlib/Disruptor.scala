7ffa9c5b2af7443c6e0a5b17e97739f89861c4ca
Arnold deVos <adv@langdale.com.au>
2015-01-16 19:00:21 +1100

    bounded queue with no back pressure and multiple subscribers

---
+++ src/main/scala/flowlib/Disruptor.scala
@@ -0,0 +1,25 @@
+package flowlib
+
+class Disruptor[T](backlog: Int) {
+  private def maxBacklog = backlog * 2
+  private case class State( buffer: Vector[T], seen: Long)
+  private val state = Transactor(State(Vector.empty, 0l))
+
+  def offer(t: T): Unit =
+    state.transact {
+      case State(buffer, seen) => 
+        val r = 
+          if(buffer.length >= maxBacklog) buffer drop buffer.length-backlog-1 
+          else buffer
+        State( r :+ t, seen + 1)
+    } { _ => }
+
+  def take( offset: Long )( k: (Long, T) => Unit): Unit =
+    state.transact {
+      case s @ State(_, seen) if offset < seen => s
+    } {
+      case State( buffer, seen ) =>
+        val ix = (offset - seen + buffer.length max 0l).toInt
+        k(ix - buffer.length + seen, buffer(ix)) 
+    } 
+}
