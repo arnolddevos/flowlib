2517e83130789e74e1e6b7bfb805a9b15add83b9
Arnold deVos <adv@langdale.com.au>
2015-02-09 13:26:07 +1100

    further refinement of producer DSL

--- src/main/scala/flowlib/Producer.scala
+++ src/main/scala/flowlib/Producer.scala
@@ -1,18 +1,23 @@
 package flowlib
 import scala.language.higherKinds
-
+ 
+import Process._
 import Producer._
 
-trait Producer[+A] {
+trait Producer[+A] { parent =>
   def step: Produced[A]
+  def +:[B >: A](b: B): Produced[B] = stop(Some(b, parent))
 }
 
 object Producer extends Transducers {
-  import Process._
 
   type Context[+S] = Process[S]
   def inContext[S](s: S) = stop(s)
   def mapContext[S, T](p: Process[S])( f: S => T ) = p map f
+
+  implicit def producerEducer = new Educer[Producer] {
+    def step[A]( p: Producer[A]) = p.step
+  }
   
   implicit def isEducible[R[_]](implicit e: Educer[R]) = new Educible[R] {
     def educe[A, S](ra: R[A], f: Reducer[A, S]): Process[S] = {
@@ -35,15 +40,9 @@ object Producer extends Transducers {
 
   type Produced[+A] = Process[Option[(A, Producer[A])]] // same as Educed[A, Producer[A]]
 
+  val nothingProduced: Produced[Nothing] = stop(None)
+
   def producer[A](p: Produced[A]) = new Producer[A] { def step = p }
 
-  def emit[A](value: A)(resume: Producer[A]): Produced[A] = stop(Some((value, resume)))
-
-  val emitEnd: Produced[Nothing] = stop(None)
-
-  val emptyProducer: Producer[Nothing] = producer(emitEnd)
-
-  implicit def producerEducer = new Educer[Producer] {
-    def step[A]( p: Producer[A]) = p.step
-  }
+  val emptyProducer: Producer[Nothing] = producer(nothingProduced)
 }
