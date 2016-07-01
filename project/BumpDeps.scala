b94334658c69be15e901bb041d337a46dc1f49c8
Arnold deVos <adv@langdale.com.au>
2016-07-01 15:42:49 +1000

    rewite BumpDeps to search up the dir tree 2 levels

--- project/BumpDeps.scala
+++ project/BumpDeps.scala
@@ -2,19 +2,31 @@ import sbt._
 import Keys._
 
 object BumpDep extends Build {
-
   def bumpDeps = Command.command("bumpDeps") { state =>
-    for(dep <- IO.listFiles(file("."), "*.sbt")) { 
-      val ext = file(s"../${dep.base}/target/${dep.name}")
-      if( ext.exists) {
+    val updates =
+      for{
+        dep <- IO.listFiles(file("."), "*.sbt")
+        ext <- {
+          for {
+            level <- 1 to 2
+            prefix = "../"*level
+            ext = file(s"$prefix${dep.base}/target/${dep.name}")
+            if ext.exists
+          }
+          yield ext
+        }.take(1)
+      }
+      yield (dep, ext)
+
+    if(updates.nonEmpty) {
+      for((dep, ext) <- updates) {
         println(s"dependency: $ext")
         IO.copyFile(ext, dep)
       }
+      state.reload
     }
-    state.reload
+    else state
   }
-
-
 
   override lazy val settings = super.settings ++ Seq(
     commands ++= Seq(bumpDeps)
