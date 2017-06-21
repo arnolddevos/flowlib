821e20052fc4d0ef18c776ece2ebf447750f247f
Arnold deVos <adv@langdale.com.au>
2017-06-21 13:26:39 +1000

    split SiteForkJoin from DefaultSite

--- src/main/scala/flowlib/DefaultSite.scala
+++ src/main/scala/flowlib/DefaultSite.scala
@@ -2,19 +2,24 @@ package flowlib
 
 import java.util.concurrent.ForkJoinPool
 import Site._
+import DefaultSite._
 
-trait DefaultSite extends Site with Monitored with SiteFailFast with SiteLogAll
+trait DefaultSite extends Site with SiteForkJoin with SiteFailFast with SiteLogAll
 
 object DefaultSite {
 
   lazy val forkJoin = new ForkJoinPool
 
-  def apply(printer: String => Unit = println _, hook: Throwable => Unit = _ => ()): DefaultSite = new DefaultSite {
-    def log(m: String) = printer(m)
-    def shutdown(e: Throwable) = hook(e)
-    def executor = forkJoin
+  trait SiteForkJoin extends Monitored { this: Site with SiteForkJoin =>
+    def executor: ForkJoinPool
     def backlog = executor.getActiveThreadCount
     def quota = executor.getParallelism
     def waiters = executor.getQueuedTaskCount.toInt
+  }
+
+  def apply(printer: String => Unit = println _, hook: Throwable => Unit = _ => ()): DefaultSite = new DefaultSite {
+    def executor = forkJoin
+    def log(m: String) = printer(m)
+    def shutdown(e: Throwable) = hook(e)
   }
 }
